02ebb540cbd4fa94596b1bdbe76c15533cce07bf
Christopher Peterson <3893680+cspeterson@users.noreply.github.com>
2024-08-19 10:53:13 -0400

    Bump version to v0.1.1

--- check_soa_serials/__init__.py
+++ check_soa_serials/__init__.py
@@ -1,6 +1,6 @@
 """ check_soa_serials """
 
 __author__ = "cspeterson"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __title__ = "check_soa_serials"
 __license__ = "License :: OSI Approved :: MIT License"
